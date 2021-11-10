7fec64d0225a60b16c27303355aecd437479343b
BagdiAron <aronpatrik30@yahoo.com>
2021-11-10 15:11:44 +0200

    commit

--- tests/test_functionalitati.py
+++ tests/test_functionalitati.py
@@ -1,11 +1,12 @@
+from UI.ui import handle_undo, handle_redo, handle_new_list
 from tests.tests import get_data
 from Logic.crud import delete, create, update, read
 from Logic.gen_change import *
-from Logic.nr_titluri import distinct_titles
+from Logic.nr_titluri import *
 from Logic.ordonare import *
 from Logic.pret_min import *
-from Logic.discount_type import reducere_pret
-from UI.clc import command_line_console
+from Logic.discount_type import *
+from UI.clc import *
 
 
 def test_reducere_pret():
@@ -36,21 +37,36 @@ def test_modificare_gen_dupa_titlu():
     assert get_gen(lista[2]) != get_gen(lista_noua[2])
 
 
-def test_distinct_titles():
+def test_undo():
     lista=get_data()
-    nr_titluri,genuri=distinct_titles(lista)
-    assert nr_titluri[0] == 2
-    assert nr_titluri[1] == 1
-    assert genuri[0] == 'lectura'
-# def test_undo():
-#     list_versions=[]
-#     lista=get_data()
-#     list_versions.append(lista)
-#     curent_version=0
-#     lista_noua=delete(lista,1)
-#     list_versions.append(lista_noua)
+    current_version = 0
+    list_versions = [lista]
+    lista_noua = delete(lista,1)
+    list_versions,current_version=handle_new_list(list_versions,current_version,lista_noua)
+    assert current_version == 1
+    assert len(list_versions)==2
+    assert len(list_versions[current_version])==3
+    lista_noua,current_version=handle_undo(list_versions,current_version)
+    assert current_version == 0
+    assert len(lista_noua)==4
 
 
+def test_redo():
+    lista=get_data()
+    current_version = 0
+    list_versions = [lista]
+    lista_noua = delete(lista,1)
+    list_versions,current_version=handle_new_list(list_versions,current_version,lista_noua)
+    assert current_version == 1
+    assert len(list_versions)==2
+    assert len(list_versions[current_version])==3
+    lista_noua,current_version=handle_undo(list_versions,current_version)
+    assert current_version == 0
+    assert len(lista_noua)==4
+    lista_noua, current_version = handle_redo(list_versions, current_version)
+    assert current_version == 1
+    assert len(list_versions) == 2
+    assert len(list_versions[current_version]) == 3
 
 
 def test_all():
@@ -58,5 +74,6 @@ def test_all():
     test_min_price_by_gen()
     test_ordonare()
     test_modificare_gen_dupa_titlu()
-    test_distinct_titles()
+    test_undo()
+    test_redo()
 test_all()
